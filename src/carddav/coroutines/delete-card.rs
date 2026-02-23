a40ffec1cb4d7884537b599a1215f75cd0bd960d
Frank Dierolf <frank_dierolf@web.de>
2026-02-23 15:01:26 +0100

    fix: handle empty 204 responses in delete operations
    Switch DeleteCard and DeleteAddressbook from Send<Response> to
    Send<Empty> to handle servers (like Stalwart) that return 204
    No Content with an empty body on DELETE requests.
    
    This matches the existing pattern used by CreateCard.

--- src/carddav/coroutines/delete-card.rs
+++ src/carddav/coroutines/delete-card.rs
@@ -1,12 +1,11 @@
 use io_stream::io::StreamIo;
-use serde::Deserialize;
 
-use crate::carddav::{config::CarddavConfig, request::Request, response::StatusResponse};
+use crate::carddav::{config::CarddavConfig, request::Request};
 
-use super::send::{Send, SendOk, SendResult};
+use super::send::{Empty, Send, SendResult};
 
 #[derive(Debug)]
-pub struct DeleteCard(Send<Response>);
+pub struct DeleteCard(Send<Empty>);
 
 impl DeleteCard {
     const BODY: &'static str = "";
@@ -23,23 +22,7 @@ impl DeleteCard {
         Self(Send::new(request, Self::BODY.as_bytes().to_vec()))
     }
 
-    pub fn resume(&mut self, arg: Option<StreamIo>) -> SendResult<bool> {
-        let ok = match self.0.resume(arg) {
-            SendResult::Ok(ok) => ok,
-            SendResult::Err(err) => return SendResult::Err(err),
-            SendResult::Io(io) => return SendResult::Io(io),
-        };
-
-        SendResult::Ok(SendOk {
-            request: ok.request,
-            response: ok.response,
-            keep_alive: ok.keep_alive,
-            body: ok.body.response.status.is_success(),
-        })
+    pub fn resume(&mut self, arg: Option<StreamIo>) -> SendResult<Empty> {
+        self.0.resume(arg)
     }
-}
-
-#[derive(Clone, Debug, Deserialize)]
-pub struct Response {
-    pub response: StatusResponse,
 }
